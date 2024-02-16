49d245a258c152418cd158b5a972e92561815e9d
NazarSheremet <87720583+NazarSheremet@users.noreply.github.com>
2024-02-16 15:04:57 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -39,16 +39,16 @@ CREATE TABLE ProductInventory (
 );
 --rollback DROP TABLE ProductInventory;
 
---changeset mate.academy:5 label:0.0.2
+--changeset ladmael:1 labels:0.0.2
 CREATE TABLE Users (
     ID INT,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     Email VARCHAR(50),
-    PRIMARY KEY (ID)
+    PRIMARY KEY(ID)
 );
 --rollback DROP TABLE Users;
 
---changeset mate.academy:6 label:0.0.3
+--changeset ladmael:2 labels:0.0.3
 CREATE INDEX Email ON Users (Email);
---rollback ALTER TABLE Users DROP INDEX Email;
+--rollback DROP INDEX Email ON Users;
