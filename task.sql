818a5e22d8682c4d8d1b8243b58c43c6e88683f0
alexh <olexandrgauptf@gmail.com>
2024-03-03 23:03:33 +0200

    Solution

--- task.sql
+++ task.sql
@@ -38,7 +38,7 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
---changeset ladmael:1 labels:0.0.2
+--changeset mate.acamemy:5 labels:0.0.2
 CREATE TABLE Users (
     ID INT,
     FirstName VARCHAR(50),
@@ -46,5 +46,7 @@ CREATE TABLE Users (
     Email VARCHAR(50),
     PRIMARY KEY(ID)
 );
---changeset ladmael:2 labels:0.0.3
+--rollback DROP TABLE Users;
+-changeset mate.acamemy:6 labels:0.0.3
 CREATE INDEX Email ON Users (Email);
+--rollback DROP INDEX Email ON Users;
