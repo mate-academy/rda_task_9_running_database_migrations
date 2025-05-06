09bf1d1245a3ebd872af6180ebcb890355c3d777
Taras Savchyn <savik1992@gmail.com>
2025-05-07 00:12:47 +0300

    delete redutant

--- task.sql
+++ task.sql
@@ -50,8 +50,5 @@ CREATE TABLE Users (
 --rollback DROP TABLE Users;
 
 --changeset mate.acamemy:6 labels:0.0.3
--- Перевірка існування таблиці перед індексом
---if exists (select * from information_schema.tables where table_name = 'Users') then
-    CREATE INDEX idx_users_email ON Users(Email);
---end if;
+CREATE INDEX idx_users_email ON Users(Email);
 --rollback DROP INDEX idx_users_email ON Users;
