59b817864d60a1da6e4f6d0ed5dc5bc3c063690b
Taras Savchyn <savik1992@gmail.com>
2025-05-07 00:09:37 +0300

    add time

--- task.sql
+++ task.sql
@@ -50,5 +50,8 @@ CREATE TABLE Users (
 --rollback DROP TABLE Users;
 
 --changeset mate.acamemy:6 labels:0.0.3
-CREATE INDEX idx_users_email ON Users(Email);
+-- Перевірка існування таблиці перед індексом
+--if exists (select * from information_schema.tables where table_name = 'Users') then
+    CREATE INDEX idx_users_email ON Users(Email);
+--end if;
 --rollback DROP INDEX idx_users_email ON Users;
