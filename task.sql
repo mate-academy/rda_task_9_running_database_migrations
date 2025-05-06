0c6e83a218657a789902d536325228049ac3747e
Taras Savchyn <savik1992@gmail.com>
2025-05-07 00:24:14 +0300

    delete redutant

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset mate.academy:1 labels:0.0.1
+--changeset mate.acamemy:1 labels:0.0.1
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
@@ -8,7 +8,7 @@ CREATE TABLE Countries (
 );
 --rollback DROP TABLE Countries;
 
---changeset mate.academy:2 labels:0.0.1
+--changeset mate.acamemy:2 labels:0.0.1
 CREATE TABLE Products (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
@@ -16,7 +16,7 @@ CREATE TABLE Products (
 );
 --rollback DROP TABLE Products;
 
---changeset mate.academy:3 labels:0.0.1
+--changeset mate.acamemy:3 labels:0.0.1
 CREATE TABLE Warehouses (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
@@ -27,7 +27,7 @@ CREATE TABLE Warehouses (
 );
 --rollback DROP TABLE Warehouses;
 
---changeset mate.academy:4 labels:0.0.1
+--changeset mate.acamemy:4 labels:0.0.1
 CREATE TABLE ProductInventory (
     ID INT,
     ProductID INT,
@@ -39,19 +39,21 @@ CREATE TABLE ProductInventory (
 );
 --rollback DROP TABLE ProductInventory;
 
---changeset mate.academy:5 labels:0.0.2
+--changeset mate.acamemy:5 labels:0.0.2
 CREATE TABLE Users (
     ID INT AUTO_INCREMENT,
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
-    Email VARCHAR(255),
+    Email VARCHAR(255) UNIQUE,
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE Users;
 
---changeset mate.academy:6 labels:0.0.3
-<preConditions onFail="MARK_RAN">
-    <tableExists tableName="Users"/>
-</preConditions>
+--changeset mate.acamemy:6 labels:0.0.3
+
 CREATE INDEX IF NOT EXISTS idx_users_email ON Users(Email);
---rollback DROP INDEX IF EXISTS idx_users_email ON Users;
+--rollback DROP INDEX idx_users_email ON Users;
+
+--changeset mate.acamemy:7 labels:0.0.3
+CREATE INDEX idx_users_email ON Users(Email);
+--rollback DROP INDEX idx_users_email ON Users;
