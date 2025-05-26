ad8e9017a8758c657b0df7206feeff7cec79b5c3
Alex S. <aleks.shaparenko@gmail.com>
2025-05-26 14:14:46 +0200

    Solution2

--- task.sql
+++ task.sql
@@ -39,7 +39,7 @@ CREATE TABLE ProductInventory (
 );
 --rollback DROP TABLE ProductInventory;
 
---changeset mate.acamemy:4 labels:0.0.2
+--changeset mate.acamemy:5 labels:0.0.2
 CREATE TABLE Users (
     ID INT,
     FirstName VARCHAR(50),
@@ -49,6 +49,6 @@ CREATE TABLE Users (
 );
 --rollback DROP TABLE Users;
 
---changeset mate.acamemy:5 labels:0.0.3
+--changeset mate.acamemy:6 labels:0.0.3
 CREATE INDEX idx_email ON Users(Email);
 --rollback DROP INDEX idx_email ON Users;
