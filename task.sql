85da2a3dd0d0d91bbeb9b7a4e25471bf7c117fb9
Danylo Povar <danila74povar@gmail.com>
2026-01-31 18:55:18 +0200

    task_9

--- task.sql
+++ task.sql
@@ -38,3 +38,16 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
+
+--changeset Povar.Danylo:5 labels:0.0.2
+CREATE TABLE Users (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL
+);
+--rollback DROP TABLE Users;
+
+--changeset Povar.Danylo:6 labels:0.0.3
+CREATE INDEX idx_user_email ON Users(Email);
+--rollback DROP INDEX idx_user_email ON Users;
