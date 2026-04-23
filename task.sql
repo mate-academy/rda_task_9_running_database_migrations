cf009654c423c8f15aedf1be2569e4d256708232
dimonalek <dimonalek@gmail.com>
2026-04-23 12:31:45 +0300

    Created additional changesets

--- task.sql
+++ task.sql
@@ -38,3 +38,17 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
+
+--changeset mate.academy:5 labels:0.0.2
+CREATE TABLE Users (
+    ID INT AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    PRIMARY KEY (ID)
+)
+--rollback DROP TABLE Users;
+
+--changeset mate.academy:6 labels:0.0.3
+CREATE index idx_email on Users (Email);
+--rollback DROP INDEX idx_email on Users;
