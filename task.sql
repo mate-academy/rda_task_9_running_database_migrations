d4f16d8e813a91517d6d6a57635ccb77a33ce878
dimonalek <dimonalek@gmail.com>
2026-04-23 12:40:03 +0300

    Fixed additional changesets

--- task.sql
+++ task.sql
@@ -46,9 +46,9 @@ CREATE TABLE Users (
     LastName VARCHAR(50),
     Email VARCHAR(50),
     PRIMARY KEY (ID)
-)
+);
 --rollback DROP TABLE Users;
 
 --changeset mate.academy:6 labels:0.0.3
-CREATE index idx_email on Users (Email);
---rollback DROP INDEX idx_email on Users;
+CREATE index Email on Users (Email);
+--rollback DROP INDEX Email on Users;
