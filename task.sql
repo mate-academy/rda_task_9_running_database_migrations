e8a613b93d2859b6cf06adc155160ba0143ea85b
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-05-18 14:07:33 +0200

    fix(migration): Changed the --rollback command for 0.0.3

--- task.sql
+++ task.sql
@@ -51,4 +51,4 @@ CREATE TABLE Users (
 
 --changeset mate.acamemy:6 labels:0.0.3
 CREATE INDEX Email ON Users(Email);
---rollback DROP INDEX idx_users_email ON Users;
+--rollback DROP Email ON Users;
