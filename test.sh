c02b60ce91b0503c2778666270d7b9398011bb20
Hrechukha-Oleksii <Oleksii_Hrechukha@outlook.com>
2026-08-04 11:21:48 +0300

    Solution5

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ echo "INFO: Wainting for database server to initialize"
 sleep 15 
 
 echo "INFO: Creating a database for test"
-docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/1-create-database.sql'
+docker exec mysql sh -c 'mysql -u root -p P@ssw0rd < /scripts/test-queries/1-create-database.sql'
 
 ### v0.0.1
 echo "INFO: Running the database migration 0.0.1"
@@ -30,7 +30,7 @@ docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_
     liquibase/liquibase liquibase tag 0.0.1 /bin/bash  
 
 echo "INFO: Running the tests for database schema version 0.0.1"
-docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/2-test-0.0.1.sql' > log.txt
+docker exec mysql sh -c 'mysql -u root -p P@ssw0rd < /scripts/test-queries/2-test-0.0.1.sql' > log.txt
 errors=$(cat log.txt | grep "^Error" || true)
 if [ -n "$errors" ]; then echo $errors && exit 1; fi
 
@@ -50,7 +50,7 @@ docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_
     liquibase/liquibase liquibase tag 0.0.2 /bin/bash
 
 echo "INFO: Running the tests for database schema version 0.0.2"
-docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/3-test-0.0.2.sql' > log.txt
+docker exec mysql sh -c 'mysql -u root -p P@ssw0rd < /scripts/test-queries/3-test-0.0.2.sql' > log.txt
 errors=$(cat log.txt | grep "^Error" || true)
 if [ -n "$errors" ]; then echo $errors && exit 1; fi
 
@@ -70,7 +70,7 @@ docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_
     liquibase/liquibase liquibase tag 0.0.3 /bin/bash
 
 echo "INFO: Running the tests for database schema version 0.0.3"
-docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/3-test-0.0.3.sql' > log.txt
+docker exec mysql sh -c 'mysql -u root -p P@ssw0rd < /scripts/test-queries/3-test-0.0.3.sql' > log.txt
 errors=$(cat log.txt | grep "^Error" || true)
 if [ -n "$errors" ]; then echo $errors && exit 1; fi
 
@@ -83,7 +83,7 @@ docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_
     liquibase/liquibase rollback 0.0.2 /bin/bash
 
 echo "INFO: Running the tests for database schema version 0.0.2"
-docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/3-test-0.0.2.sql' > log.txt
+docker exec mysql sh -c 'mysql -u root -p P@ssw0rd < /scripts/test-queries/3-test-0.0.2.sql' > log.txt
 errors=$(cat log.txt | grep "^Error" || true)
 if [ -n "$errors" ]; then echo $errors && exit 1; fi
 
@@ -93,9 +93,9 @@ docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_
     -e LIQUIBASE_COMMAND_USERNAME=root \
     -e LIQUIBASE_COMMAND_PASSWORD=P@ssw0rd \
     -e LIQUIBASE_COMMAND_URL=jdbc:mysql://mysql:3306/ShopDB \
-    liquibase/liquibase rollback 0.0.1 /bin/bash
+    liquibase/liquibase rollback 0.0.1
 
 echo "INFO: Running the tests for database schema version 0.0.1"
-docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/2-test-0.0.1.sql' > log.txt
+docker exec mysql sh -c 'mysql -u root -p P@ssw0rd < /scripts/test-queries/2-test-0.0.1.sql' > log.txt
 errors=$(cat log.txt | grep "^Error" || true)
 if [ -n "$errors" ]; then echo $errors && exit 1; fi
