0d30b0841df5770de2a0bee857885b962c38110d
Daniil <shpak.daniil06@gmail.com>
2026-09-09 01:41:33 +0300

    Add database migrations

--- task.sql
+++ task.sql
@@ -38,3 +38,17 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
+
+--changeset daniil:5 labels:0.0.2
+CREATE TABLE Users(
+    ID INT,
+    FirstName VARCHAR (50),
+    LastName VARCHAR (50),
+    Email VARCHAR (100)
+);
+--rollback DROP TABLE Users;
+
+--changeset daniil:6 labels:0.0.3
+CREATE INDEX index_email
+ON Users (Email);
+--rollback DROP INDEX index_email ON Users;
