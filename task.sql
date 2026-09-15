c8073bb141f173fbd478489d41e782ec08a3d4c0
Taras <eostasarat@gmail.com>
2026-09-15 15:48:04 +0300

    Revert index name to 'Email'

--- task.sql
+++ task.sql
@@ -50,5 +50,5 @@ CREATE TABLE Users (
 --rollback DROP TABLE Users;
 
 --changeset mate.acamemy:6 labels:0.0.3
-CREATE INDEX users_email_idx ON Users (Email);
---rollback DROP INDEX users_email_idx ON Users;
+CREATE INDEX Email ON Users (Email);
+--rollback DROP INDEX Email ON Users;
